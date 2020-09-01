c44912ffe5c9961d426a2fe353d3a22581ef9429
lamvuhang <hanglam.24hdev@gmail.com>
2020-09-01 22:09:45 +0700

    post/get in axios

---
+++ vue-axios/src/components/dashboard/dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  created() {
+    axios.get('https://vuejs-axios-bab32.firebaseio.com/users.json')
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+}
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
