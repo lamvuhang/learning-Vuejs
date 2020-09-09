163f39c36a87d7896112a7d750a105e146f814a5
lamvuhang <hanglam.24hdev@gmail.com>
2020-09-09 21:22:31 +0700

    vuelidate

--- vue-axios/src/components/auth/signup.vue
+++ vue-axios/src/components/auth/signup.vue
@@ -2,12 +2,14 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{invalid: $v.email.$error}">
           <label for="email">Mail</label>
           <input
                   type="email"
                   id="email"
+                  @input="$v.email.$touch()"
                   v-model="email">
+          <p v-if="$v.email.$error">Pls enter valid email address</p>
         </div>
         <div class="input">
           <label for="age">Your Age</label>
@@ -70,6 +72,7 @@
 
 <script>
 import axios from '../../axios-auth'
+import { required, email} from 'vuelidate/lib/validators'
 
   export default {
     data () {
@@ -81,6 +84,12 @@ import axios from '../../axios-auth'
         country: 'usa',
         hobbyInputs: [],
         terms: false
+      }
+    },
+    validations: {
+      email: {
+        required: required,
+        email: email
       }
     },
     methods: {
@@ -149,6 +158,15 @@ import axios from '../../axios-auth'
     outline: none;
     border: 1px solid #521751;
     background-color: #eee;
+  }
+
+  .input.invalid label{
+    color: red;
+  }
+
+  .input.invalid input{
+    border: 1px solid red;
+    background-color: rosybrown;
   }
 
   .input select {
--- vue-axios/src/main.js
+++ vue-axios/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import vuelidate from'vuelidate'
 
 import router from './router'
 import store from './store'
+
+Vue.use(vuelidate)
 
 axios.defaults.baseURL = 'https://vuejs-axios-bab32.firebaseio.com'
 
